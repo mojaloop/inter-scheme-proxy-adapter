91d6f644c87294a23b19365d834c59a2b65550bb
geka.evk <geka.evk@gmail.com>
2024-06-12 16:27:23 +0100

    feat(csi-48): added tsconfig for tests

--- test/unit/domain/ISPAService.test.ts
+++ test/unit/domain/ISPAService.test.ts
@@ -24,11 +24,11 @@
  **********/
 
 import { ISPAService, ISPAServiceInterface } from '#src/domain';
-import { loggerFactory } from '#src/utils';
 import { PROXY_HEADER, AUTH_HEADER } from '#src/constants';
+import { loggerFactory } from '#src/utils';
 import config from '#src/config';
 
-// import * as fixtures from '#test/fixtures';
+import * as fixtures from '#test/fixtures';
 
 describe('ISPAService Tests -->', () => {
   const logger = loggerFactory('test');
@@ -43,7 +43,7 @@ describe('ISPAService Tests -->', () => {
     const incomingUrl = new URL(`http://localhost:12345${pathWithSearchParams}`);
     const headers = { h: 'h1' };
     const { baseUrl } = config.get('hubBConfig');
-    const token = 'TOKEN_NOT_IMPLEMENTED_YET'; // todo: update after implement token retrieval
+    const token = fixtures.mockBearerToken(); // todo: update after implement token retrieval
 
     const proxyDetails = service.getProxyTarget({
       url: incomingUrl,
